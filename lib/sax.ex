fa5dcc34a18b24c1e631a561fd8fdf04593ef203
Stian Håklev <shaklev@gmail.com>
2015-05-12 12:48:03 +0800

    cleaning up, and moving tracing out

--- lib/sax.ex
+++ lib/sax.ex
@@ -2,15 +2,20 @@ defmodule Sax do
   import MultiDef
 
   def parse(str) do
-    case :erlsom.parse_sax(str, [], &(respond/2), []) do
+    case :erlsom.parse_sax(str, [], &(respond_tr/2), []) do
       {:ok, [x], []} -> {:ok, x}
       {:ok, list, []} -> {:ok, Enum.reverse(list)}
     end
   end
 
-  def trace(args, state) do
-    IO.inspect(args)
-    state
+  def respond_tr(:startDocument, _) do 
+    IO.puts("\n\n#{String.duplicate("#", 50)}\n")
+    {:wait, []}
+  end
+
+  def respond_tr(args, state) do
+    IO.inspect([args, state])
+    respond(args, state)
   end
 
   # TODO: right now handles structs as individual params OK, but cannot merge structs with multiple members, 
@@ -19,15 +24,15 @@ defmodule Sax do
 
   mdef respond do 
     :startDocument,_                          -> {:wait, []}
-    {:startElement, [], elem, [], []}, state  -> trace(elem, startElem(elem, state))
+    {:startElement, [], elem, [], []}, state  -> startElem(elem, state)
     :endDocument, {_, result}                 -> result
     
-    {:characters, str}, {:value, acc}         -> trace(str,{:wait, [List.to_string(str) | acc] })
-    {:characters, str}, {:string, acc}        -> trace(str, {:wait, [List.to_string(str) | acc] })
-    {:characters, str}, {:int, acc}           -> trace(str, {:wait, [List.to_integer(str) | acc] })
-    {:characters, str}, {:float, acc}         -> trace(str, {:wait, [List.to_float(str) | acc] })
-    {:characters, str}, {:boolean, acc}       -> trace(str, {:wait, [to_boolean(str) | acc] })
-    {:characters, str}, {:name, acc}          -> trace(str, {:struct_val, List.to_string(str), acc })
+    {:characters, str}, {:value, acc}         -> {:wait, [List.to_string(str) | acc] }
+    {:characters, str}, {:string, acc}        -> {:wait, [List.to_string(str) | acc] }
+    {:characters, str}, {:int, acc}           -> {:wait, [List.to_integer(str) | acc] }
+    {:characters, str}, {:float, acc}         -> {:wait, [List.to_float(str) | acc] }
+    {:characters, str}, {:boolean, acc}       -> {:wait, [to_boolean(str) | acc] }
+    {:characters, str}, {:name, acc}          -> {:struct_val, List.to_string(str), acc }
 
     # for struct values
     {:characters, str}, {:value, name, acc}   -> {:wait, [Map.put(%{}, name, List.to_string(str)) | acc] }
@@ -36,7 +41,7 @@ defmodule Sax do
     {:characters, str}, {:float, name, acc}   -> {:wait, [Map.put(%{}, name, List.to_float(str)) | acc] }
     {:characters, str}, {:boolean, name, acc} -> {:wait, [Map.put(%{}, name, to_boolean(str)) | acc] }
 
-    x, state                                  -> trace({:respond_missing, x, state}, state)
+    x, state                                  -> state
   end
 
   mdef startElem do
@@ -60,7 +65,7 @@ defmodule Sax do
     'i4',    {:value, name, acc}       -> {:int, name, acc}
     'boolean',    {:value, name, acc}  -> {:boolean, name, acc}
     'float',    {:value, name, acc}    -> {:float, name, acc}
-    str, state = {fsm, _}              -> trace({str,fsm}, state)
+    _, state                           -> state
   end
 
   mdef to_boolean do
